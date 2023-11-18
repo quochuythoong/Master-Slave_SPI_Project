3e07d99ccaf8656d3344f1b59ac268b0e44bd767
quochuythoong <140998907+quochuythoong@users.noreply.github.com>
2023-11-18 18:57:15 +0700

    CHIP_MASTER asm

--- main.asm
+++ main.asm
@@ -1,156 +1,245 @@
 ;
-; CHIP_SLAVE.asm
+; CHIP_MASTER.asm
 ;
-; Created: 11/15/2023 7:56:41 AM
+; Created: 11/15/2023 8:08:24 PM
 ; Author : tuan minh
 ;
 
 
 ; Replace with your application code
-.ORG 0
-.EQU SIGNAL = 0
+.ORG $0000
+		RJMP MAIN
+
+.ORG $0002 ; EXTERNAL INTERRUPT REQUEST 0 ADDRESS FOR ATMEGA324PA (LAB)
+		RJMP INT0_HANDLER
+
 .EQU PIN_SS = 4
 .EQU PIN_MOSI = 5
 .EQU PIN_MISO = 6
 .EQU PIN_SCK = 7
 .EQU SPI_DDR = DDRB
 .EQU SPI_PORT = PORTB
-.EQU SPI_PIN = PINB
-.EQU KEY_PORT = PORTD ; KEY DATA PORT
-.EQU KEY_PIN = PIND ; KEY DATA PIN
-.EQU KEY_DDR = DDRD ; KEY DATA DDR
-.EQU REQ = 1
+.EQU LCD_PORT = PORTA ; LCD DATA PORT
+.EQU LCD_DDR = DDRA  ; LCD DATA DDR
+.EQU LCD_PIN = PINA  ; LCD DATA PIN
+.EQU LCD_RS = 0		  ; LCD RS
+.EQU LCD_RW = 1       ; LCD RW
+.EQU LCD_EN = 2       ; LCD EN
+.EQU REQ = 0 ; COMMUNICATION REQUEST AFTER DONE SETUP DONE
 .DEF TEMP = R16
 .DEF DATA_TEMP = R17
 
 MAIN:
+		SBI SPI_PORT, REQ
 		CALL INIT_PORT
-		CALL INIT_SSPI
+		CALL INIT_LCD
+		CALL INIT_MSPI ; INITIALIZE MASTER SPI
+		CALL INIT_UART0
+		CALL INIT_INT0
 
 LOOP:
-		SBIC SPI_PIN, REQ
-		RJMP LOOP
-		SBI SPI_PORT, SIGNAL
-		RCALL SCANKEY
-		;CPI DATA_TEMP, 16
-		;BREQ PASS
-		RCALL DATA_CONVERT
-
-;PASS:
-		RCALL SSPI_RECEIVE
+		CBI PORTB, REQ
 		RJMP LOOP
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 INIT_PORT:
-		LDI TEMP, (1 << PIN_MISO) | (1 << SIGNAL)
+		LDI TEMP, 0b11110111
+		OUT LCD_DDR, TEMP ; SET OUTPUT PORT TO LCD (DATA PA4 - PA7, RS = PA0, RW = PA1, EN = PA2)
+		CALL DELAY_20ms ; WAIT FOR POWER UP
+		RET
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+INIT_LCD:
+		LDI TEMP, 0x02 ; RETURN HOME
+		CALL CMDWRITE
+		LDI TEMP, 0x28 ; FUCNTION SET: 4-BIT, 2 LINES, 5x7 DOTS
+		CALL CMDWRITE
+		LDI TEMP, 0x0E ; DISPLAY ON, CURSOR ON
+		CALL CMDWRITE
+		LDI TEMP, 0x01 ; CLEAR DISPLAY SCREEN
+		CALL CMDWRITE
+		LDI TEMP, 0x80 ; FORCE CURSOR TO BEGIN OF 1ST ROW
+		CALL CMDWRITE
+		LDI R31, HIGH(LAB_MSG0 << 1)
+		LDI R30, LOW(LAB_MSG0 << 1)
+
+STRING0: 
+		LPM TEMP, Z+
+		CPI TEMP, 0
+		BREQ NEXT_LINE
+		RCALL DATAWRITE
+		RJMP STRING0
+
+NEXT_LINE: 
+		LDI TEMP, 0xC0 ; FORCE CURSOR TO BEGIN OF 2ND ROW
+		RCALL CMDWRITE
+		RET
+
+LAB_MSG0: .DB "KEY PRESSED: ", 0
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+INIT_MSPI:
+		; ENABLE SPI MASTER, RATE FCK/16
+		LDI TEMP, (1 << PIN_MOSI) | (1 << PIN_SCK) | (1 << PIN_SS) | (1 << REQ)
 		OUT SPI_DDR, TEMP
-		LDI TEMP, 0x0F
-		OUT KEY_DDR, TEMP ; SET PC0-3 OUTPUT COLUMN, PC4-7 INPUT ROW 
-		LDI TEMP, 0xF0
-		OUT KEY_PORT, TEMP ; PULL-UP
-		RET
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-INIT_SSPI:
-		LDI TEMP, (1 << SPE0)
+		LDI TEMP, (1 << SPE0) | (1 << MSTR0) | (1 << SPR00)
 		OUT SPCR0, TEMP
+		SBI SPI_PORT, PIN_SS
 		RET
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-SSPI_RECEIVE:
-		;CALL INIT_SSPI
-		OUT SPDR0, DATA_TEMP ; STORE KEY VALUE IN SLAVE
-		CBI SPI_PORT, SIGNAL 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+MSPI_TRANSMIT:
+		CBI SPI_PORT, PIN_SS
+		OUT SPDR0, DATA_TEMP
 
-SSPI_WAIT_RECEIVE:
+WAIT_TRANSMIT:
+		; WAIT TRANSMISSION COMPLETE
 		IN TEMP, SPSR0
-		SBRS TEMP, SPIF0 ; WAIT FOR RECEPTION FROM MASTER
-		RJMP SSPI_WAIT_RECEIVE
-		SBI SPI_PORT, SIGNAL
+		SBRS TEMP, SPIF0
+		RJMP WAIT_TRANSMIT
+		SBI SPI_PORT, REQ
+		IN DATA_TEMP, SPDR0
 		RET
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-SCANKEY:
-		LDI R20, 0x0F
-		OUT KEY_PORT, R20
-		LDI R22, 0b11110111
-		LDI R23, 0
-		LDI R24, 3
-
-KEYPAD_SCAN_LOOP:
-		OUT KEY_PORT, R22
-		CALL DELAY_10ms
-		SBIC KEY_PIN, 4
-		RJMP KEYPAD_SCAN_CHECK_COL_2
-		RJMP KEYPAD_SCAN_FOUND
-
-KEYPAD_SCAN_CHECK_COL_2:
-		SBIC KEY_PIN, 5
-		RJMP KEYPAD_SCAN_CHECK_COL_3
-		LDI R23, 1
-		RJMP KEYPAD_SCAN_FOUND
-
-KEYPAD_SCAN_CHECK_COL_3:
-		SBIC KEY_PIN, 6
-		RJMP KEYPAD_SCAN_CHECK_COL_4
-		LDI R23, 2
-		RJMP KEYPAD_SCAN_FOUND
-
-KEYPAD_SCAN_CHECK_COL_4:
-		SBIC KEY_PIN, 7
-		RJMP KEYPAD_SCAN_NEXT_ROW
-		LDI R23, 3
-		RJMP KEYPAD_SCAN_FOUND
-
-KEYPAD_SCAN_NEXT_ROW:
-		CPI R24, 0
-		BREQ KEYPAD_SCAN_NOT_FOUND
-		ROR R22
-		DEC R24
-		RJMP KEYPAD_SCAN_LOOP
-
-KEYPAD_SCAN_FOUND:
-		LSL R23
-		LSL R23
-		ADD R23, R24 ; KEYPAD VALUE IN R23
-		MOV DATA_TEMP, R23
-		RET
-
-KEYPAD_SCAN_NOT_FOUND:
-		LDI DATA_TEMP, 0xFF
-		RET
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-DATA_CONVERT:
-		LDI TEMP, HIGH(ARRAY << 1)
-		MOV ZH, TEMP
-		LDI TEMP, LOW(ARRAY << 1)
-		MOV ZL, TEMP
-		ADD ZL, DATA_TEMP
-		LPM DATA_TEMP, Z
-		RET
-
-ARRAY:
-.DB '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 0xFF
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-DELAY_10ms:
-		LDI TEMP, 0b00001010 ; CTC MODE, CLK/8
-		STS TCCR1B, TEMP
-		LDI TEMP, 0x27
-		STS OCR1AH, TEMP
-		LDI TEMP, 0x10
-		STS OCR1AL, TEMP
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+INIT_UART0:
 		CLR TEMP
-		STS TCNT1H, TEMP
-		STS TCNT1L, TEMP
-
-DELAY10ms_LOOP:
-		SBIS TIFR1, OCF1A
-		RJMP DELAY10ms_LOOP 
-		SBI TIFR1, OCF1A ; RESET TIMER1
-		CLR TEMP
-		STS TCCR1A, TEMP
-		STS TCCR1B, TEMP
+		STS UBRR0H, TEMP
+		LDI TEMP, 51
+		STS UBRR0L, TEMP
+		LDI TEMP, (1 << RXEN0) | (1 << TXEN0) ; ENABLE RECEPTION/TRANSMISSION
+		STS UCSR0B, TEMP
+		LDI TEMP, (1 << UCSZ01) | (1 << UCSZ00) ; ASYNC, 1 STOP-BIT, 1-BYTE DATA
+		STS UCSR0C, TEMP
 		RET
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+DATA_TRANSMIT:
+		LDS TEMP, UCSR0A 
+		SBRS TEMP, UDRE0
+		RJMP DATA_TRANSMIT
+		STS UDR0, DATA_TEMP
+		RET
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+DATA_RECEIVE:
+		LDS TEMP, UCSR0A
+		SBRS TEMP, RXC0
+		RJMP DATA_RECEIVE
+		LDS DATA_TEMP, UDR0
+		;OUT PORTB, TEMP_DATA
+		RET
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+INIT_INT0:
+		SEI
+		LDI TEMP, (1 << ISC01) ; INT 0 FALLING EDGE 
+		STS EICRA, TEMP
+		LDI TEMP, (1 << INT0)
+		OUT EIMSK, TEMP
+		RET
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+CMDWRITE:
+		RCALL DELAY_20ms
+		MOV R18, TEMP
+		ANDI R18, 0xF0 ; MASK LOW NIBBLE
+		OUT LCD_PORT, R18    ; SEND HIGH NIBBLE
+		SBI LCD_PORT, LCD_EN ; EN = 1 HIGH PULSE
+		RCALL SDELAY          ; EXTEND EN PULSE
+		CBI LCD_PORT, LCD_EN ; EN=0 FOR H-to-L PULSE
+		RCALL DELAY_100us     
+
+		SWAP TEMP
+		ANDI TEMP, 0xF0 ; MASK HIGH NIBBLE
+		OUT LCD_PORT, TEMP    ; SEND LOW NIBBLE
+		SBI LCD_PORT, LCD_EN ; EN = 1 FOR HIGH PULSE
+		RCALL SDELAY          ; EXTEND EN PULSE
+		CBI LCD_PORT, LCD_EN ; EN=0 FOR H-to-L PULSE
+		RCALL DELAY_100us
+		RET
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+DATAWRITE:
+		RCALL DELAY_20ms
+		MOV R18, TEMP
+		ANDI R18, 0xF0 ; MASK LOW NIBBLE
+		OUT LCD_PORT, R18    ; SEND HIGH NIBBLE
+		SBI LCD_PORT, LCD_RS ; RS = 1 TO DATA
+		SBI LCD_PORT, LCD_EN ; EN = 1 HIGH PULSE
+		RCALL SDELAY          ; EXTEND EN PULSE
+		CBI LCD_PORT, LCD_EN ; EN=0 FOR H-to-L PULSE
+		RCALL DELAY_100us 
+
+		SWAP TEMP
+		ANDI TEMP, 0xF0 ; MASK HIGH NIBBLE
+		OUT LCD_PORT, TEMP    ; SEND LOW NIBBLE
+		SBI LCD_PORT, LCD_RS ; RS = 1 TO DATA
+		SBI LCD_PORT, LCD_EN ; EN = 1 HIGH PULSE
+		RCALL SDELAY          ; EXTEND EN PULSE
+		CBI LCD_PORT, LCD_EN ; EN=0 FOR H-to-L PULSE
+		RCALL DELAY_100us   
+		RET
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+SDELAY:
+		NOP
+		NOP
+		RET
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+DELAY_100us:
+		PUSH R17
+		LDI R17,100
+DR0: 
+		CALL SDELAY
+		DEC R17
+		BRNE DR0
+		POP R17
+		RET
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+DELAY_2ms:
+		PUSH R17
+		LDI R17,20
+LDR0: 
+		CALL DELAY_100us
+		DEC R17
+		BRNE LDR0
+		POP R17
+		RET
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+DELAY_20ms:
+		PUSH R17
+		LDI R17, 10
+POWERUP:
+		CALL DELAY_2ms
+		DEC R17
+		BRNE POWERUP
+		POP R17
+		RET
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;EXTERNAL INTERRUPT 0 REQUEST HANDLER;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+INT0_HANDLER:	
+		LDI DATA_TEMP, '0' ; RANDOM DATA	
+		RCALL MSPI_TRANSMIT
+		SBI SPI_PORT, REQ
+		CPI DATA_TEMP, 0xFF
+		BREQ CLEAR
+		MOV TEMP, DATA_TEMP 
+		RCALL DATAWRITE ; DISPLAY KEY CODE TO LCD
+		LDI TEMP, 0xC0 ; FORCE CURSOR TO BEGINNING OF 2ND LINE
+		RCALL CMDWRITE
+		RJMP NEXT
+
+CLEAR:
+		LDI TEMP, 0x20 ; ASCII CODE OF SPACE CHAR
+		RCALL DATAWRITE
+		LDI TEMP, 0xC0 ; FORCE CURSOR TO BEGINNING OF 2ND LINE 
+		RCALL CMDWRITE
+
+NEXT:
+		;CALL DATA_TRANSMIT
+		;CALL DATA_RECEIVE
+		LDI TEMP, (1 << INTF0)
+		OUT EIFR, TEMP 
+		RETI
